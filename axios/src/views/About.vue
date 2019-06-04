fddf4e45223bc610fa0d6daf3986a82e2cb49c1c
wmhello <871228582@qq.com>
2019-06-04 21:02:34 +0800

    http请求

---
+++ axios/src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <h1>项目说明</h1>
+  </div>
+</template>
---
+++ axios/src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>首页</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import axios from 'axios'
+
+export default {
+  name: 'home',
+  created () {
+    axios({
+      method: 'get',
+      url: '/orders',
+      baseURL: 'https://www.easy-mock.com/mock/5cd03a89e97f9709cf42a9dc/test/',
+      withCredentials: true,
+      params: {
+        name: '张三'
+      }
+    }).then (res => {
+      console.log('响应')
+      console.log(res.data)
+    }).catch (err => {
+    })
+  }
+}
+</script>
